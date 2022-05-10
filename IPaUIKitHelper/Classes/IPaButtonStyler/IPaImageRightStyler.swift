1b472ac5c99d896776bd6a5a49e183f2ae35a3df
IPa Chen <ipapamagic@gmail.com>
2022-05-10 14:28:34 +0800

    add a offset for IPaImageRightStyler , to fix sometime it will clip title

--- IPaUIKitHelper/Classes/IPaButtonStyler/IPaImageRightStyler.swift
+++ IPaUIKitHelper/Classes/IPaButtonStyler/IPaImageRightStyler.swift
@@ -17,18 +17,19 @@ open class IPaImageRightStyler:IPaButtonStyler {
         button.contentEdgeInsets = UIEdgeInsets(top: button.contentEdgeInsets.top, left: 0, bottom: button.contentEdgeInsets.bottom, right: 0)
     }
     open override func reloadStyle(_ button: UIButton) {
-        guard let imageView = button.imageView,let image = imageView.image,let titleLabel = button.titleLabel,let text = titleLabel.text,let font = button.titleLabel?.font else {
+        guard let imageView = button.imageView,let titleLabel = button.titleLabel,let text = titleLabel.text,let font = button.titleLabel?.font else {
             return
         }
-        let textLabelWidth = button.bounds.width - image.size.width - leftSpace - rightSpace
-        let textWidth = min(textLabelWidth,(text as NSString).size(withAttributes: [.font:font]).width)
-        let imageWidth = image.size.width
+        let imageWidth = imageView.bounds.width
+        let textLabelWidth = button.bounds.width - imageWidth - leftSpace - rightSpace
+        let textWidth = min(textLabelWidth,(text as NSString).size(withAttributes: [.font:font]).width) + 5
+        
         
         let space = (centerSpace <= 0) ? (button.bounds.width - leftSpace - rightSpace - imageWidth - textWidth) : centerSpace
         
         let halfSpace = max(0,space * 0.5)
         let titleLeft = -imageWidth - halfSpace
-        button.titleEdgeInsets = UIEdgeInsets(top: button.titleEdgeInsets.top, left: titleLeft, bottom: button.titleEdgeInsets.bottom, right: (space >= 0) ? -titleLeft : -titleLeft - space)
+        button.titleEdgeInsets = UIEdgeInsets(top: button.titleEdgeInsets.top, left: titleLeft, bottom: button.titleEdgeInsets.bottom, right: -titleLeft)
         let imageLeft = textWidth + halfSpace + ((space >= 0) ? 0 : space)
         button.imageEdgeInsets = UIEdgeInsets(top: button.imageEdgeInsets.top, left: imageLeft, bottom: button.imageEdgeInsets.bottom, right: -imageLeft)
         button.contentEdgeInsets = UIEdgeInsets(top: button.contentEdgeInsets.top, left: halfSpace + leftSpace, bottom: button.contentEdgeInsets.bottom, right: halfSpace + rightSpace)
