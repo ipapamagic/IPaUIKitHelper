eec8b53a6d00f4626cde9f3865e3e2905afdcf6c
IPaPa <ipapamagic@gmail.com>
2022-07-08 19:44:15 +0800

    fix the  IPaImageRightStyler for button styler

--- IPaUIKitHelper/Classes/IPaButtonStyler/IPaImageRightStyler.swift
+++ IPaUIKitHelper/Classes/IPaButtonStyler/IPaImageRightStyler.swift
@@ -11,7 +11,6 @@ open class IPaImageRightStyler:IPaButtonStyler {
     @IBInspectable open var centerSpace: CGFloat = 0
     @IBInspectable open var leftSpace: CGFloat = 0
     @IBInspectable open var rightSpace: CGFloat = 0
-    @IBInspectable open var textWidthOffset: CGFloat = 0
     open override func clearStyle(_ button:UIButton) {
         button.titleEdgeInsets = UIEdgeInsets(top: button.titleEdgeInsets.top, left: 0, bottom: button.titleEdgeInsets.bottom, right: 0)
         button.imageEdgeInsets = UIEdgeInsets(top: button.imageEdgeInsets.top, left: 0, bottom: button.imageEdgeInsets.bottom, right: 0)
@@ -23,21 +22,35 @@ open class IPaImageRightStyler:IPaButtonStyler {
         guard let imageView = button.imageView,let titleLabel = button.titleLabel,let text = titleLabel.text,let font = button.titleLabel?.font else {
             return
         }
-        let imageWidth = imageView.bounds.width
-        let buttonSize = button.systemLayoutSizeFitting(button.intrinsicContentSize)
-        let textLabelWidth = buttonSize.width - imageWidth
-        let textWidth = min(textLabelWidth,(text as NSString).size(withAttributes: [.font:font]).width) + textWidthOffset
+        let imageWidth = imageView.intrinsicContentSize.width
+        let textWidth = (text as NSString).size(withAttributes: [.font:font]).width
+        var space = centerSpace
+        var titleLeft = -imageWidth
+        var imageLeft = textWidth
+        var contentEdgeInsets = UIEdgeInsets.zero
+        var halfSpace = space
+        if centerSpace > 0 {
+            halfSpace = max(0,space * 0.5)
+            contentEdgeInsets = UIEdgeInsets(top: button.contentEdgeInsets.top, left: leftSpace + halfSpace, bottom: button.contentEdgeInsets.bottom, right: rightSpace + halfSpace)
+        }
+        else {
+            //dynamic center space
+            let buttonSize = button.bounds.size
+            space = (buttonSize.width  - imageWidth - textWidth - leftSpace - rightSpace)
+            titleLeft += leftSpace
+            imageLeft -= rightSpace
+            
+            if space > 0 {
+                let extraSpace = space * 0.5
+                contentEdgeInsets = UIEdgeInsets(top: button.contentEdgeInsets.top, left: extraSpace, bottom: button.contentEdgeInsets.bottom, right: extraSpace)
+            }
+            halfSpace = max(0,space * 0.5)
+        }
         
-        
-        let space = ((centerSpace <= 0) ? (buttonSize.width  - imageWidth - textWidth) : centerSpace)
-        
-        let halfSpace = max(0,space * 0.5)
-        let titleLeft = -imageWidth - halfSpace
+        titleLeft -= halfSpace
+        imageLeft += halfSpace
         button.titleEdgeInsets = UIEdgeInsets(top: button.titleEdgeInsets.top, left: titleLeft, bottom: button.titleEdgeInsets.bottom, right: -titleLeft)
-        let imageLeft = textWidth + halfSpace
         button.imageEdgeInsets = UIEdgeInsets(top: button.imageEdgeInsets.top, left: imageLeft, bottom: button.imageEdgeInsets.bottom, right: -imageLeft)
-        
-        let halfTextWidthOffset = textWidthOffset * 0.5
-        button.contentEdgeInsets = UIEdgeInsets(top: button.contentEdgeInsets.top, left: halfSpace + leftSpace + halfTextWidthOffset, bottom: button.contentEdgeInsets.bottom, right: halfSpace + rightSpace + halfTextWidthOffset)
+        button.contentEdgeInsets = contentEdgeInsets
     }
 }
