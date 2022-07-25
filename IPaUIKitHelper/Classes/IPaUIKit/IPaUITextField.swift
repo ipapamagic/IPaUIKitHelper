406fd242af400e56a740b3efdb937c3737539c6f
IPaPa <ipapamagic@gmail.com>
2022-07-26 02:15:23 +0800

    implement textInsets / editingInset / contentInset for set IPaUITextField EdgeInsets

--- IPaUIKitHelper/Classes/IPaUIKit/IPaUITextField.swift
+++ IPaUIKitHelper/Classes/IPaUIKit/IPaUITextField.swift
@@ -9,46 +9,127 @@
 import UIKit
 //@IBDesignable
 open class IPaUITextField: UITextField {
-    
     //@IBInspectable
     @objc public var bottomInset: CGFloat {
         get {
-            return textInsets.bottom
+            return self.contentInsets.bottom
         }
         set {
-            textInsets.bottom = newValue
+            var insets = self.contentInsets
+            insets.bottom = newValue
+            self.contentInsets = insets
         }
     }
     //@IBInspectable
     @objc public var leftInset: CGFloat {
         get {
-            return textInsets.left
+            return self.contentInsets.left
         }
         set {
-            textInsets.left = newValue
+            var insets = self.contentInsets
+            insets.left = newValue
+            self.contentInsets = insets
         }
     }
     //@IBInspectable
     @objc public var rightInset: CGFloat {
         get {
-            return textInsets.right
+            return self.contentInsets.right
         }
         set {
-            textInsets.right = newValue
+            var insets = self.contentInsets
+            insets.right = newValue
+            self.contentInsets = insets
         }
     }
     //@IBInspectable
     @objc public var topInset: CGFloat {
         get {
-            return textInsets.top
+            return self.contentInsets.top
         }
         set {
-            textInsets.top = newValue
+            var insets = self.contentInsets
+            insets.top = newValue
+            self.contentInsets = insets
         }
     }
-    
+    @objc public var editingRightInset:CGFloat {
+        get {
+            return self.editingInsets.right
+        }
+        set {
+            self.editingInsets.right = newValue
+        }
+    }
+    @objc public var editingLeftInset:CGFloat {
+        get {
+            return self.editingInsets.left
+        }
+        set {
+            self.editingInsets.left = newValue
+        }
+    }
+    @objc public var editingTopInset:CGFloat {
+        get {
+            return self.editingInsets.top
+        }
+        set {
+            self.editingInsets.top = newValue
+        }
+    }
+    @objc public var editingBottomInset:CGFloat {
+        get {
+            return self.editingInsets.bottom
+        }
+        set {
+            self.editingInsets.bottom = newValue
+        }
+    }
+    @objc public var textRightInset:CGFloat {
+        get {
+            return self.textInsets.right
+        }
+        set {
+            self.textInsets.right = newValue
+        }
+    }
+    @objc public var textLeftInset:CGFloat {
+        get {
+            return self.textInsets.left
+        }
+        set {
+            self.textInsets.left = newValue
+        }
+    }
+    @objc public var textTopInset:CGFloat {
+        get {
+            return self.textInsets.top
+        }
+        set {
+            self.textInsets.top = newValue
+        }
+    }
+    @objc public var textBottomInset:CGFloat {
+        get {
+            return self.textInsets.bottom
+        }
+        set {
+            self.textInsets.bottom = newValue
+        }
+    }
     //@IBInspectable
+    @objc public var contentInsets:UIEdgeInsets {
+        get {
+            return self.textInsets
+        }
+        set {
+            textInsets = newValue
+            editingInsets = newValue
+        }
+    }
+    @objc public var editingInsets: UIEdgeInsets = UIEdgeInsets.zero
     @objc public var textInsets: UIEdgeInsets = UIEdgeInsets.zero
+    
     public override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -56,14 +137,14 @@ open class IPaUITextField: UITextField {
         super.init(coder: aDecoder)
         
     }
-    
-    
     override open func textRect(forBounds bounds: CGRect) -> CGRect {
-        return bounds.inset(by: textInsets)
+        let originalRect = super.textRect(forBounds: bounds)
+        return originalRect.inset(by: textInsets)
     }
     
     override open func editingRect(forBounds bounds: CGRect) -> CGRect {
-        return textRect(forBounds: bounds)
+        let originalRect = super.editingRect(forBounds: bounds)
+        return originalRect.inset(by: editingInsets)
     }
     override open func placeholderRect(forBounds: CGRect) -> CGRect {
         return textRect(forBounds: bounds)
